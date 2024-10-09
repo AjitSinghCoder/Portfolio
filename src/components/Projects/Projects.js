7e78829460ddb2ef72becc1e36c0001b81a5795e
AjitSinghCoder <ajitkrsingh841@gmail.com>
2024-10-09 22:09:33 +0530

    feat: new project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,11 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
+import dylog from "../../Assets/Projects/dylog.png";
 import bitsOfCode from "../../Assets/Projects/blog.png";
 import bookyourpandit from "../../Assets/Projects/bookyourpandit.webp";
 
@@ -30,7 +27,7 @@ function Projects() {
               description="A multi-platform app for booking religious services and ceremonies."
               // ghLink="https://github.com/soumyajit4419/Chatify"
               demoLink="https://play.google.com/store/apps/details?id=com.bookyourpandit.client&hl=en_IN&pli=1"
-              skills={["Python", "Django", "React", "Tailwind"]}
+              skills={["Python", "Django", "NextJs", "React", "Typescript"]}
             />
           </Col>
 
@@ -38,55 +35,28 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="Morsmiles"
+              description="Mor Smiles presents an extensive directory of doctors, spanning diverse medical fields. Fromcardiologists to dermatologists, find the expert you need to address your specific health concerns."
               demoLink="https://blogs.soumya-jit.tech/"
+              skills={[
+                "Python",
+                "Django",
+                "FastAPI",
+                "NextJs",
+                "React",
+                "Typescript",
+              ]}
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={editor}
+              imgPath={dylog}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={suicide}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+              title="Dylog Chatbot"
+              description="Dylog is a chatbot designed for the B2B sector, helping improve communication between buyers and suppliers in industries like hardware products and defense services."
+              demoLink="https://www.dylog.ai/"
+              skills={["Python", "Rasa", "OpenAI"]}
             />
           </Col>
         </Row>
